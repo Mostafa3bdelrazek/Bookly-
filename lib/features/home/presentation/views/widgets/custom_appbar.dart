b94e57f19130802c85c5d2c1ccfae6c51bc6acb6
mostafa abdelrazek <mostafaabdelrazrk.com>
2024-07-17 12:33:25 +0300

    Add custom AppBar to home page

---
+++ lib/features/home/presentation/views/widgets/custom_appbar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsHelper.logo,
+            height: 18.h,
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24.sp,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'features/splash/presentation/views/splash_view.dart';
@@ -12,12 +13,17 @@ class Bookly extends StatelessWidget {
   const Bookly({super.key});
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: baseColor,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: GetMaterialApp(
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: baseColor,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const SplashView(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const SplashView(),
     );
   }
 }
