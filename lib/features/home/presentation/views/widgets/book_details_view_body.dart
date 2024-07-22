65cd7367a51960bfdbb905d466406ce2ca2a7ac0
mostafa abdelrazek <mostafaabdelrazrk.com>
2024-07-22 18:23:52 +0300

    add title & subtitle to book details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,8 @@
+import 'package:bookly/core/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'custom_book_details_view_app_bar.dart';
 
@@ -15,9 +18,29 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomBookDetailsViewAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
             child: const CustomBookImage(),
           ),
+          SizedBox(
+            height: 43.h,
+          ),
+          Text(
+            'the jungle book',
+            style: Styles.textStyle30,
+          ),
+          SizedBox(
+            height: 6.h,
+          ),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          )
         ],
       ),
     );
