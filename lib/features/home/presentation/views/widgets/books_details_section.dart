d6e65a321b9cc23f1ee76d7f58a0982f3c7c2879
mostafa abdelrazek <mostafaabdelrazrk.com>
2024-07-23 18:19:26 +0300

    refactor books details view ui

---
+++ lib/features/home/presentation/views/widgets/books_details_section.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
+import 'book_action.dart';
+import 'book_rating.dart';
+import 'custom_book_image.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        Text(
+          'the jungle book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 18,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 37,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: BookAction(),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/similar_books_section.dart
@@ -0,0 +1,31 @@
+
+import 'package:bookly/features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
+
+class SimilarBooksSection extends StatelessWidget {
+  const SimilarBooksSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'You can also like',
+          style: Styles.textStyle14.copyWith(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.15,
+          child: const SimilarBooksListView(),
+        ),
+      ],
+    );
+  }
+}
