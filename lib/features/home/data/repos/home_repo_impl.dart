e18cd2eb21e764fabf39c7dc196caacc6fc08ebc
mostafa abdelrazek <mostafaabdelrazrk.com>
2024-07-24 15:17:19 +0300

    Create api service

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,16 @@
+import 'package:bookly/core/error_handler.dart/failure.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+}
