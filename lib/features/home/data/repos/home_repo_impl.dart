6b1a7acfd3662a08ede95f97c6e41e35367a729d
mostafa abdelrazek <mostafaabdelrazrk.com>
2024-07-26 17:07:30 +0300

    impl fetch similar books

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -13,8 +13,7 @@ class HomeRepoImpl implements HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
     try {
       var data = await apiService.get(
-        endPoint:
-            'volumes?Filtering=free-ebooks&q=flutter &Sorting=newest',
+        endPoint: 'volumes?Filtering=free-ebooks&q=flutter &Sorting=newest',
       );
       List<BookModel> books = [];
       for (var item in data['items']) {
@@ -39,6 +38,27 @@ class HomeRepoImpl implements HomeRepo {
     try {
       var data = await apiService.get(
         endPoint: 'volumes?Filtering=free-ebooks&q=computer science',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category}) async {
+    try {
+      var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=flutter &Sorting=relevance',
       );
       List<BookModel> books = [];
       for (var item in data['items']) {
